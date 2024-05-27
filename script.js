7ab2aea69c35d95657e5516e09759d0b2f610af0
Priyanshu Semwal <65777631+Priyanshu2425@users.noreply.github.com>
2024-05-27 17:41:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 const extendHex = (shortHex) => {
-  // write your code here
-};
+  let longHex = "#";
+	for(let i = 0; i < shortHex.length; i++){
+		if(shortHex[i] === "#"){
+			continue;
+		}
+		longHex += shortHex[i] + shortHex[i];
+	}
+	return longHex;
+}; 
 
 // Do not change the code below.
 const shortHex = prompt("Enter Short Hex.");
